8be1ce1323bd1713302e980032db6f7deef4040c
HayatoDoi <b1517914@planet.kanazawa-it.ac.jp>
2017-08-09 04:33:26 +0900

    [debug commit] change url

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ new Vue({
     text: []
   },
   mounted() {
-    axios.get("/markdown/mark")
+    axios.get("https://hayatodoi.github.io/markdown/mark")
     .then(response => {this.text = response.data.results;console.log(this.text);})
   }
 });
